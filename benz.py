947de9cc7d5fdf4924f6af5b2d7600e48f7c7681
Pavika <54035413+pavikachaudhary@users.noreply.github.com>
2020-07-09 17:46:31 +0530

    new files 3

---
+++ benz.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.speed(60)
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x/100 + 1)
+    t.forward(x)
+    t.circle(x*2)
+    t.left(59)
